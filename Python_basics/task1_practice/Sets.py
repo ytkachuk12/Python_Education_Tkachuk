b7a4ac87cf01e8201cd5ccd3e4c45842f3abbc1c
Yurii Tkachuk <ytkachuk12@gmail.com>
2021-09-18 22:01:38 +0200

    Task1_practice. Added all task files

---
+++ Python_basics/task1_practice/Sets.py
@@ -0,0 +1,7 @@
+"""In the exercise below, use the given lists to print out a set containing all the participants from event
+A which did not attend event B."""
+
+a = ["Jake", "John", "Eric"]
+b = ["John", "Jill"]
+
+print(set(a).difference(set(b)))
