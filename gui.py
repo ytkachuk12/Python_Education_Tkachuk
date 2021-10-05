ae2d671f38b78096ec9d60d6cb7628acdf242ff3
Yurii Tkachuk <ytkachuk12@gmail.com>
2021-10-05 18:39:33 +0300

    ver 1. console, without gui

---
+++ gui.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+def menu_window():
+    window = tk.Tk
+
+    game_menu = tk.Label(text="Tic Tac Toe")
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    #menu_window()
+
+    window = tk.Tk
+
+    game_menu = tk.Label(text="Tic Tac Toe")
+    window.mainloop()
